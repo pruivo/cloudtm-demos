17ac422bb351afa5efceb9e562f5aa5c0da82d95
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2013-01-18 16:17:17 +0000

    * added barrier to know when the cluster is formed (scenario 3)

--- scenario3/src/main/resources/run.sh
+++ scenario3/src/main/resources/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+echo "Expected one argument: <number of nodes expected>"
+exit 0;
+fi
+
 WORKING_DIR=`cd $(dirname $0); pwd`;
 MAIN_CLASS="test.MainApp"
 JVM_OPTIONS="-Xmx1G"
@@ -14,7 +19,7 @@ do
 CP="${CP}:${jar}";
 done
 
-CMD="java ${JVM_OPTIONS} ${D_VARS} -cp ${CP} ${MAIN_CLASS} ${HOSTNAME}"
+CMD="java ${JVM_OPTIONS} ${D_VARS} -cp ${CP} ${MAIN_CLASS} ${HOSTNAME} $1"
 
 echo $CMD
 eval $CMD
